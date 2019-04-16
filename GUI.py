063c7fbc6aa25cf3f4a5f0acb2c51d8fa8d177e9
SylviaCHS <hc239@duke.edu>
2019-04-16 15:58:17 -0400

    Import file label

--- GUI.py
+++ GUI.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from tkinter import ttk
-from tkinter import filedialog
 
 
 def design_window():
@@ -24,6 +23,15 @@ def design_window():
     new_factor = StringVar()
     th_check = ttk.Checkbutton(root, text='New User', variable=new_factor, onvalue=True, offvalue=False)
     th_check.grid(column=1, row=2)
+
+    def import_file():
+        from tkinter import filedialog
+        filename = filedialog.askopenfilename(initialdir="/", title="Select file",
+                                                   filetypes=(("jpeg files", "*.jpeg"), ("all files", "*.*")))
+        print(filename)
+
+    import_btn = ttk.Button(root, text='Import File(s)', command=import_file)
+    import_btn.grid(column=2, row=1)
 
     root.mainloop()  # Shows window
 
