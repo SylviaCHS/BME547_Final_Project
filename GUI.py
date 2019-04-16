c88b1ad730f9477271402a49139c1beb16dde0d2
SylviaCHS <hc239@duke.edu>
2019-04-16 14:48:50 -0400

    Add user_name label #2

--- GUI.py
+++ GUI.py
@@ -13,6 +13,13 @@ def design_window():
     top_label = ttk.Label(root, text="Image Processor")
     top_label.grid(column=0, row=0)
 
+    name_label = ttk.Label(root, text="User Name:")
+    name_label.grid(column=0, row=1)
+
+    user_name = StringVar()
+    name_entry = ttk.Entry(root, textvariable=user_name)
+    name_entry.grid(column=1, row=1)
+
     root.mainloop()  # Shows window
 
 
