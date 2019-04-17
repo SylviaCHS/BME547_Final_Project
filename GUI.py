74a1e6ae3c244a2bd1435aadf2ee93ee3ccc0e7d
SylviaCHS <hc239@duke.edu>
2019-04-16 20:37:29 -0400

    Add import_file to GUI, fix pep8 format #2

--- GUI.py
+++ GUI.py
@@ -2,39 +2,47 @@ from tkinter import *
 from tkinter import ttk
 
 
-def design_window():
-    def ok_function():
+class GUI:
+    def __init__(self, root):
+        def ok_function():
 
-        pass
+            pass
 
-    root = Tk()  # Makes main window
-    root.title('Image Processor GUI')
+        root.title('Image Processor GUI')
 
-    top_label = ttk.Label(root, text="Image Processor")
-    top_label.grid(column=0, row=0)
+        top_label = ttk.Label(root, text="Image Processor")
+        top_label.grid(column=0, row=0)
 
-    name_label = ttk.Label(root, text="User Name:")
-    name_label.grid(column=0, row=1)
+        name_label = ttk.Label(root, text="User Name:")
+        name_label.grid(column=0, row=1)
 
-    user_name = StringVar()
-    name_entry = ttk.Entry(root, textvariable=user_name)
-    name_entry.grid(column=1, row=1)
+        self.user_name = StringVar()
+        name_entry = ttk.Entry(root, textvariable=self.user_name)
+        name_entry.grid(column=1, row=1)
 
-    new_factor = StringVar()
-    th_check = ttk.Checkbutton(root, text='New User', variable=new_factor, onvalue=True, offvalue=False)
-    th_check.grid(column=1, row=2)
+        self.new_factor = StringVar()
+        th_check = ttk.Checkbutton(root, text='New User',
+                                   variable=self.new_factor,
+                                   onvalue=True, offvalue=False)
+        th_check.grid(column=1, row=2)
 
-    def import_file():
+        self.filename = StringVar()
+        import_btn = ttk.Button(root, text='Import File(s)',
+                                command=lambda: self.import_file())
+        import_btn.grid(column=2, row=1)
+
+    def import_file(self):
         from tkinter import filedialog
-        filename = filedialog.askopenfilename(initialdir="/", title="Select file",
-                                                   filetypes=(("jpeg files", "*.jpeg"), ("all files", "*.*")))
-        print(filename)
-
-    import_btn = ttk.Button(root, text='Import File(s)', command=import_file)
-    import_btn.grid(column=2, row=1)
-
-    root.mainloop()  # Shows window
+        self.filename = filedialog.askopenfilename(
+            initialdir="/", title="Select file",
+            filetypes=(("JPEG files", "*.jpeg"),
+                       ("PNG files", "*.png"),
+                       ("TIFF files", "*.tiff"),
+                       ("ZIP files", "*.zip"),
+                       ("all files", "*.*")))
 
 
 if __name__ == '__main__':
-    design_window()
+    root = Tk()  # Makes main window
+    gui = GUI(root)
+    root.mainloop()  # Shows window
